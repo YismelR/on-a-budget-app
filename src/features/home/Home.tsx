4f6c45559840afe2b27f3f42859a065987ed7c90
YismelR <yismel_rodriguez@outlook.com>
2024-07-25 16:34:29 -0400

    added a height for the root 100vh, added the footer button, added a scroll Area for the the list of transactions

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -5,8 +5,8 @@ import plusSign from "@/assets/icons/plus.svg";
 import WelcomeDisplay from "./WelcomeDisplay";
 export default function Home() {
   return (
-    <>
-      <div className="p-4">
+    <div className="flex flex-col h-full">
+      <div className="px-4 pt-4 h-full">
         <header className="flex justify-between pt-6 pb-5">
           <WelcomeDisplay />
         </header>
@@ -15,9 +15,11 @@ export default function Home() {
           <TransactionList />
         </main>
       </div>
-      <footer className="bg-white h-10">
-        <img src={plusSign} alt="plus sign" />
+      <footer className="bg-white flex h-20 rounded-t-3xl relative">
+        <div className=" absolute bottom-8 left-40 p-3 bg-gradient-to-br from-[#243B33] to-[#7C8C7C] rounded-full">
+          <img src={plusSign} alt="plus sign" className=" size-7 " />
+        </div>
       </footer>
-    </>
+    </div>
   );
 }
--- src/features/home/TransactionList.tsx
+++ src/features/home/TransactionList.tsx
@@ -1,4 +1,5 @@
 import { expensesList } from "@/listOfExpenses/expensesList";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavLink } from "react-router-dom";
 export default function TransactionList() {
   return (
@@ -9,27 +10,29 @@ export default function TransactionList() {
           <p className="font-medium text-[#A1B3C8] cursor-pointer">View All</p>
         </NavLink>
       </div>
-      {expensesList.map((expenses, id) => {
-        return (
-          <div
-            className="flex justify-between bg-white p-4 rounded-2xl shadow-md"
-            key={id}
-          >
-            <div className="flex place-items-center gap-3">
-              <div
-                className={`${expenses.bgColor} bg-opacity-15 p-3 rounded-full`}
-              >
-                <img src={expenses.src} alt={expenses.alt} />
+      <ScrollArea className="h-[240px] rounded-md ">
+        {expensesList.map((expenses, id) => {
+          return (
+            <div
+              className="flex justify-between bg-white p-4 rounded-2xl shadow-md mb-4"
+              key={id}
+            >
+              <div className="flex place-items-center gap-3">
+                <div
+                  className={`${expenses.bgColor} bg-opacity-15 p-3 rounded-full`}
+                >
+                  <img src={expenses.src} alt={expenses.alt} />
+                </div>
+                <h1 className="font-semibold">{expenses.title}</h1>
               </div>
-              <h1 className="font-semibold">{expenses.title}</h1>
+              <div className="text-right">
+                <p>-${expenses.Total}</p>
+                <p className="text-[#A1B3C8]">{expenses.date}</p>
+              </div>
             </div>
-            <div className="text-right">
-              <p>-${expenses.Total}</p>
-              <p className="text-[#A1B3C8]">{expenses.date}</p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </ScrollArea>
     </section>
   );
 }
