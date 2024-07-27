2354d4bf4356aaf47c3ed4e4b209ea236af3bf41
YismelR <yismel_rodriguez@outlook.com>
2024-07-27 12:43:47 -0400

    updated the expenses display and applied an expenses file

--- src/features/expenses/Expenses.tsx
+++ src/features/expenses/Expenses.tsx
@@ -2,15 +2,30 @@ import { Input } from "@/components/ui/input";
 import close from "@/assets/icons/close.svg";
 import ExpensesSelection from "./ExpensesSelection";
 import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router-dom";
 
 export default function Expenses() {
   return (
-    <div className="p-6">
-      <img src={close} alt="close" />
-      <h1>Add Expenses</h1>
-      <Input type="text" placeholder="$ 0" />
-      <ExpensesSelection />
-      <Button>Save</Button>
+    <div className="p-6 h-full">
+      <div className="flex justify-end">
+        <NavLink to="/">
+          <img src={close} alt="close" />
+        </NavLink>
+      </div>
+      <div className="flex flex-col h-full place-content-center justify-evenly">
+        <h1 className="text-2xl font-semibold flex justify-center">
+          Add Expenses
+        </h1>
+        <Input
+          type="text"
+          placeholder="$ 0"
+          className="h-20 rounded-full w-72 place-self-center placeholder:text-3xl placeholder:text-center placeholder:text-[#7C8C7C] text-2xl text-center"
+        />
+        <ExpensesSelection />
+        <Button className="bg-gradient-to-br from-[#243B33] to-[#7C8C7C] hover:from-[#7C8C7C] hover:to-[#D8D3C0]">
+          Save
+        </Button>
+      </div>
     </div>
   );
 }
--- src/features/transactions/ExpensesDisplay.tsx
+++ src/features/transactions/ExpensesDisplay.tsx
@@ -1,28 +1,31 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { expensesList } from "@/listOfExpenses/expensesList";
 
 export default function ExpensesDisplay() {
   return (
     <section className="grid gap-4">
-      {expensesList.map((expenses, id) => {
-        return (
-          <div
-            className="flex justify-between bg-white p-4 rounded-2xl shadow-md"
-            key={id}
-          >
-            <div className="flex place-items-center gap-3">
-              <div
-                className={`${expenses.bgColor} bg-opacity-15 p-3 rounded-full`}
-              >
-                <img src={expenses.src} alt={expenses.alt} />
+      <ScrollArea className="h-[240px] rounded-md ">
+        {expensesList.map((expenses, id) => {
+          return (
+            <div
+              className="flex justify-between bg-white p-4 rounded-2xl shadow-md"
+              key={id}
+            >
+              <div className="flex place-items-center gap-3">
+                <div
+                  className={`${expenses.bgColor} bg-opacity-15 p-3 rounded-full`}
+                >
+                  <img src={expenses.src} alt={expenses.alt} />
+                </div>
+                <h1 className="font-semibold">{expenses.title}</h1>
               </div>
-              <h1 className="font-semibold">{expenses.title}</h1>
+              <div className="flex text-right place-items-center">
+                <p>-${expenses.Total}</p>
+              </div>
             </div>
-            <div className="flex text-right place-items-center">
-              <p>-${expenses.Total}</p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </ScrollArea>
     </section>
   );
 }
